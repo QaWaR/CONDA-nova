11e16802b44dc113d46deca428ea3d577f13ca49
Milos Pavlovic <milosar7@gmail.com>
2018-02-27 14:12:21 +0100

    Footer in progress

--- footer.php
+++ footer.php
@@ -17,12 +17,45 @@
             <div class="container">
                 <div class="content-wrapper py-2 d-flex justify-content-center align-items-baseline">
                     <p class="text-success h3 m-0 mx-2">Do you have any questions?</p>
-                    <p class="m-0 mx-2">Call us | +431 33 69 069</p>
-                    <p class="m-0 mx-2">Write us | <a href="#" class="text-white">team@conda.at</a> </p>
+                    <p class="m-0 mx-2">Call us | <i class="fas fa-phone"></i>+431 33 69 069</p>
+                    <p class="m-0 mx-2">Write us | <i class="fas fa-envelope"></i>
+<a href="#" class="text-white">team@conda.at</a> </p>
                 </div>
             </div>
         </section>
+        <section class="info bg-info">
+           <div class="container">
+               <div class="info-wrapper py-5 d-flex justify-content-around">
+                   <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                       <img src="images/logoconda.png" class="img-fluid w-75 mb-2">
+                       <p class="m-0">CONDA Crowdinvesting</p>
+                       <p class="m-0">Donau-City-Strasse 6</p>
+                       <p class="m-0">1220 Wien</p>
+                   </div>
+                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-whit border-right border-lighte">
+                      <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Email..">
+                          <span class="input-group-btn">
+                              <button class="btn btn-success" type="button">Subscribe</button>
+                          </span>
+                      </div>  
+                    </div>
+                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                        <p class="h4">Folgen sie uns auf</p>
 
+                    </div>
+                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white">
+                        <a href="#" class="text-white">Risikohinweis</a>
+                        <a href="#" class="text-white">Blog</a>
+                        <a href="#" class="text-white">Presse</a>
+                        <a href="#" class="text-white">Impressum</a>
+                        <a href="#" class="text-white">Hilfe</a>
+                    </div>
+
+               </div>
+           </div> 
+
+        </section>
 
 
 
