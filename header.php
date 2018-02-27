3f56f39706be2630de71285a6f78b40ac75cbcad
Nebojsa Nikolic <qawar321@gmail.com>
2018-02-27 13:13:19 +0100

    Add FA and header

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CONDA - Crowdinvesting</title>
+    
+    
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <section class="container">
+            <nav class="navbar navbar-expand-lg navbar-light px-0">
+                <a class="navbar-brand d-flex align-items-center justify-content-between alignt-items-center" href="index.php">
+                    <img src="imgs/conda-logo.png" alt="">
+                    <p class="mb-0">CROWDINVESTING</p>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarMain">
+                    <div class="navbar-nav ml-auto">
+                        <a href="#" class="nav-item nav-link text-uppercase"><small>projecte</small><b>entdecken</b></a>
+                        <a href="#" class="nav-item nav-link text-uppercase"><small>so funktioniert</small><b>crowdinvesting</b></a>
+                        <a href="#" class="nav-item nav-link text-uppercase"><small>projecte</small><b>einreichen</b></a>
+                        <a href="#" class="nav-item nav-link text-uppercase"><small>uber</small><b>conda</b></a>
+                    </div>
+                </div>
+            </nav>
+        </section>
+
+
+
+
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                </div>
+            </div>
+        </nav> -->
+    </header>
+</body>
+</html>
