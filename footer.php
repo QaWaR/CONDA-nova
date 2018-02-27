70d330f0452ac90694d6312ee9dccd55223f45a4
Milos Pavlovic <milosar7@gmail.com>
2018-02-27 15:54:48 +0100

    Footer finish(html, bootstrap only)

--- footer.php
+++ footer.php
@@ -1,56 +1,85 @@
         <footer>
             <div class="box bg-danger" style="height: 200px;"></div>
-        <section class="questions bg-primary py-5 text-white">
+            <section class="footer-slider bg-light py-3">
+                <div class="container">
+                    <div class="slider py-5">
+                        <div class="images-wrapper d-flex flex-row justify-content-between">
+                            <div class="image-block d-flex flex-row justify-content-between">
+                                <img class="d-block" src="images/carousel/carousel0.png" alt="First slide">
+                            </div>
+                            <div class="image-block">
+                                <img class="d-block" src="images/carousel/carousel1.png" alt="Second slide">
+                            </div>
+                            <div class="image-block">
+                                <img class="d-block" src="images/carousel/carousel2.png" alt="Third slide">
+                            </div>
+                            <div class="image-block">
+                                <img class="d-block" src="images/carousel/carousel3.png" alt="Third slide">
+                            </div>
+                            <div class="image-block">
+                                <img class="d-block" src="images/carousel/carousel4.png" alt="Third slide">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="questions bg-primary py-5 text-white">
+                <div class="container">
+                    <div class="content-wrapper py-2 d-flex flex-column flex-lg-row justify-content-lg-center justify-content-around align-items-center align-items-lg-baseline">
+                        <p class="text-success h3 m-0 mx-2">Do you have any questions?</p>
+                        <p class="m-0 mx-2">Call us | <i class="fas fa-phone mr-1"></i>+431 33 69 069</p>
+                        <p class="m-0 mx-2">Write us | <i class="fas fa-envelope mr-1"></i>
+    <a href="#" class="text-white">team@conda.at</a> </p>
+                    </div>
+                </div>
+            </section>
+            <section class="info bg-info">
             <div class="container">
-                <div class="content-wrapper py-2 d-flex justify-content-center align-items-baseline">
-                    <p class="text-success h3 m-0 mx-2">Do you have any questions?</p>
-                    <p class="m-0 mx-2">Call us | <i class="fas fa-phone"></i>+431 33 69 069</p>
-                    <p class="m-0 mx-2">Write us | <i class="fas fa-envelope"></i>
-<a href="#" class="text-white">team@conda.at</a> </p>
+                <div class="info-wrapper py-5 d-flex flex-lg-row flex-sm-column flex-column justify-content-lg-around align-items-sm-center">
+                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                        <img src="images/logoconda.png" class="w-50 mb-2 align-self-center align-self-sm-center align-self-lg-start">
+                        <p class="m-0 text-center text-sm-center text-lg-left">CONDA Crowdinvesting</p>
+                        <p class="m-0 text-center text-sm-center text-lg-left"><i class="fas fa-map-marker-alt mr-1"></i>Donau-City-Strasse 6</p>
+                        <p class="m-0 text-center text-sm-center text-lg-left"><i class="fas fa-building mr-1"></i>1220 Wien</p>
+                    </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-whit border-right border-lighte">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Email..">
+                            <span class="input-group-btn">
+                                <button class="btn btn-success" type="button">Subscribe</button>
+                            </span>
+                        </div>  
+                        </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                            <p class="h4 mb-4 text-center text-sm-center text-lg-left">Folgen sie uns auf</p>
+                            <div class="social-media-wrapper d-flex flex-row">
+                                <i class="fab fa-facebook-f mx-3"></i>
+                                <i class="fab fa-twitter mx-3"></i>
+                                <i class="fab fa-linkedin mx-3"></i>
+                                <i class="fab fa-youtube mx-3"></i>
+                                <i class="fab fa-google-plus-g mx-3"></i>
+                                <i class="fab fa-xing mx-3"></i>
+                            </div>
+                        </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white text-center text-sm-center text-lg-left">
+                            <a href="#" class="text-white">Risikohinweis</a>
+                            <a href="#" class="text-white">Blog</a>
+                            <a href="#" class="text-white">Presse</a>
+                            <a href="#" class="text-white">Impressum</a>
+                            <a href="#" class="text-white">Hilfe</a>
+                        </div>
                 </div>
             </div>
-        </section>
-        <section class="info bg-info">
-           <div class="container">
-               <div class="info-wrapper py-5 d-flex justify-content-around">
-                   <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
-                       <img src="images/logoconda.png" class="img-fluid w-75 mb-2">
-                       <p class="m-0">CONDA Crowdinvesting</p>
-                       <p class="m-0">Donau-City-Strasse 6</p>
-                       <p class="m-0">1220 Wien</p>
-                   </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-whit border-right border-lighte">
-                      <div class="input-group">
-                          <input type="text" class="form-control" placeholder="Email..">
-                          <span class="input-group-btn">
-                              <button class="btn btn-success" type="button">Subscribe</button>
-                          </span>
-                      </div>  
-                    </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
-                        <p class="h4">Folgen sie uns auf</p>
-
-                    </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white">
-                        <a href="#" class="text-white">Risikohinweis</a>
-                        <a href="#" class="text-white">Blog</a>
-                        <a href="#" class="text-white">Presse</a>
-                        <a href="#" class="text-white">Impressum</a>
-                        <a href="#" class="text-white">Hilfe</a>
-                    </div>
-
-               </div>
-           </div> 
-
-        </section>
+            </section>
 
 
 
 
 
-        <script src="js/jquery.min.js"></script>
-		<script src="js/popper.min.js"></script>
-		<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+            <script src="js/jquery.min.js"></script>
+            <script src="js/popper.min.js"></script>
+            <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+            <script src="js/main.js"></script>
         </footer>
     </body>
 </html>
