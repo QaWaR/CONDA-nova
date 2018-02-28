996c615d5887620ccc0a024253fd6b4ac517179c
Milos Pavlovic <milosar7@gmail.com>
2018-02-28 10:02:20 +0100

    Campaign IN finishing

---
+++ src/Starting-in.php
@@ -0,0 +1,52 @@
+<?php include 'header.php';?>
+<main>
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-12 col-sm-12 col-md-6 col-xl-4">
+                <a href="" class="card-clickable-link d-block mb-4">
+                    <div class="starting-in-single card">
+                        <img src="images/armywings/armywings.jpg" class="card-img-top">
+                        <div class="card-header bg-primary px-3 py-2 d-flex justify-content-between">
+                            <p class="time-left text-uppercase text-white font-weight-bold my-auto">Starting in <span class="h4"> 1 </span> month</p>
+                            <img src="images/armywings/wingslogo.jpg" class="rounded-circle" style="width:50px; height:50px;">
+                        </div>
+                        <div class="card-body text-dark bg-light">
+                            <h4 class="company-name card-title"><img src="images/armywings/aut.svg" class="img-fluid mr-1" style="width:20px;"> Army wings(CI/VMN/BC/ANd)</h4>
+                            <p class="short-summary card-text mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dignissimos a vitae recusandae amet itaque perferendis architecto non aliquam maiores. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit aspernatur possimus neque eveniet natus, illo debitis dolore dolorem sequi aperiam fuga? Voluptatum velit, repudiandae consequatur doloremque error culpa quasi itaque?</p>
+                            <hr>
+                            <div class="status-wrapper text-dark d-flex justify-content-around">
+                                <div class="status-scale rounded-circle border border-secondary p-4 pt-5 mr-3 text-center">
+                                    <p class="total-amount font-weight-bold m-0">0$</p>
+                                    <p class="numb-of-investors m-0 text-muted">0 Investors</p>
+                                    <p class="completed" >0%</p>
+                                </div>
+                                <div class="invested-amount align-self-center">
+                                    <p class="text-muted"><span class="min-amoun d-block h5 m-0 text-dark">75.000$</span> Minimum Investment</p>
+                                    <p class="text-muted"><span class="max-amount d-block h5 m-0 text-dark">700.000$</span> Maximum investment</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer p-0">
+                            <div class="tags-wrapper d-flex justify-content-around border-bottom border-muted">
+                                <div class="single-tab px-2 py-2 d-flex flex-row">
+                                    <i class="far fa-star text-muted fa-2x align-self-center"></i>
+                                </div>
+                                <div class="single-tab px-2 py-2 text-center">
+                                    <i class="fas fa-calendar-alt text-muted fa-1x mb-2"></i>
+                                    <span class="d-block text-muted font-weight-bold">2 years</span>
+                                </div>
+                            </div>
+                            <div class="footer-button py-4 text-center">
+                                <button class="btn btn-secondary text-uppercase py-2 px-4">View details</button>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-xl-4">
+                
+            </div>
+        </div>
+    </div>
+</main>
+<?php include 'footer.php';?>
