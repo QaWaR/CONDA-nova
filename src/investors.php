ebb3e8144922c168e9c82c716f1b7ea8c87373e6
Milos Pavlovic <milosar7@gmail.com>
2018-02-28 11:58:14 +0100

    Investors.php create html only

---
+++ src/investors.php
@@ -0,0 +1,18 @@
+<?php include 'header.php';?>
+<main>
+    <section class="investors-say py-5">
+        <div class="container">
+            <div class="content-wrapper">
+                <h2 class="text-center text-sm-center text-md-left mb-3">Das sagen Investoren</h2>
+                <div class="image-comment-wrapper py-3 px-5 text-center">
+                    <img src="images/investors/michael.jpg" class="mb-4 rounded-circle">
+                    <p class="h4 text-uppercase mb-4">Michael Altrrichter</p>
+                    <p class="lead text-muted px-5">"Crowdinvest ist seit geraumer Zeit immer stärker im Kommen, da es sich für viele Geschäftsmodelle, vor allem im B2C Bereich, hervorragend anbietet. Es freut mich wirklich sehr, dass es mit CONDA bei uns ein derart engagiertes Team gibt, das dieses Thema so stark fördert und in die Breite trägt!"</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</main>
+<?php include 'footer.php';?>
