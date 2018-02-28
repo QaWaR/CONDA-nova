fc5811c2752446a16d27e9745b66ab72d1a719df
Milos Pavlovic <milosar7@gmail.com>
2018-02-28 15:13:32 +0100

    Call-to-action section remake(with font awesome instead of images)

--- src/footer.php
+++ src/footer.php
@@ -29,47 +29,47 @@
                         <p class="text-success h3 m-0 mx-2">Do you have any questions?</p>
                         <p class="m-0 mx-2 text-white">Call us | <i class="fas fa-phone mr-1 text-white"></i>+431 33 69 069</p>
                         <p class="m-0 mx-2 text-white">Write us | <i class="fas fa-envelope mr-1 text-white"></i>
-    <a href="#" class="text-white">team@conda.at</a> </p>
+                            <a href="#" class="text-white">team@conda.at</a> </p>
                     </div>
                 </div>
             </section>
             <section class="info bg-info">
-            <div class="container">
-                <div class="info-wrapper py-5 d-flex flex-lg-row flex-sm-column flex-column justify-content-lg-around align-items-sm-center align-items-lg-start">
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
-                        <img src="images/logoconda.png" class="w-50 mb-2 align-self-center align-self-sm-center align-self-lg-start">
-                        <p class="m-0 text-center text-sm-center text-lg-left text-white">CONDA Crowdinvesting</p>
-                        <p class="m-0 text-center text-sm-center text-lg-left text-white"><i class="fas fa-map-marker-alt mr-1 text-white"></i>Donau-City-Strasse 6</p>
-                        <p class="m-0 text-center text-sm-center text-lg-left text-white"><i class="fas fa-building mr-1 text-white"></i>1220 Wien</p>
-                    </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-whit border-right border-lighte">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Email..">
-                            <span class="input-group-btn">
-                                <button class="btn btn-success" type="button">Subscribe</button>
-                            </span>
-                        </div>  
-                    </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
-                        <p class="h4 mb-4 text-center text-sm-center text-lg-left">Folgen sie uns auf</p>
-                        <div class="social-media-wrapper d-flex flex-row justify-content-around">
-                            <i class="fab fa-facebook-f mx-3 text-white"></i>
-                            <i class="fab fa-twitter mx-3 text-white"></i>
-                            <i class="fab fa-linkedin mx-3 text-white"></i>
-                            <i class="fab fa-youtube mx-3 text-white"></i>
-                            <i class="fab fa-google-plus-g mx-3 text-white"></i>
-                            <i class="fab fa-xing mx-3 text-white"></i>
+                <div class="container">
+                    <div class="info-wrapper py-5 d-flex flex-lg-row flex-sm-column flex-column justify-content-lg-around align-items-sm-center align-items-lg-start">
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                            <img src="images/logoconda.png" class="w-50 mb-2 align-self-center align-self-sm-center align-self-lg-start">
+                            <p class="m-0 text-center text-sm-center text-lg-left text-white">CONDA Crowdinvesting</p>
+                            <p class="m-0 text-center text-sm-center text-lg-left text-white"><i class="fas fa-map-marker-alt mr-1 text-white"></i>Donau-City-Strasse 6</p>
+                            <p class="m-0 text-center text-sm-center text-lg-left text-white"><i class="fas fa-building mr-1 text-white"></i>1220 Wien</p>
+                        </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-whit border-right border-lighte">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Email..">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-success" type="button">Subscribe</button>
+                                </span>
+                            </div>  
+                        </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white border-right border-light">
+                            <p class="h4 mb-4 text-center text-sm-center text-lg-left">Folgen sie uns auf</p>
+                            <div class="social-media-wrapper d-flex flex-row justify-content-around">
+                                <i class="fab fa-facebook-f mx-3 text-white"></i>
+                                <i class="fab fa-twitter mx-3 text-white"></i>
+                                <i class="fab fa-linkedin mx-3 text-white"></i>
+                                <i class="fab fa-youtube mx-3 text-white"></i>
+                                <i class="fab fa-google-plus-g mx-3 text-white"></i>
+                                <i class="fab fa-xing mx-3 text-white"></i>
+                            </div>
+                        </div>
+                        <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white text-center text-sm-center text-lg-left">
+                            <a href="#" class="text-white">Risikohinweis</a>
+                            <a href="#" class="text-white">Blog</a>
+                            <a href="#" class="text-white">Presse</a>
+                            <a href="#" class="text-white">Impressum</a>
+                            <a href="#" class="text-white">Hilfe</a>
                         </div>
                     </div>
-                    <div class="single-info-wrapper py-3 px-3 d-flex flex-column text-white text-center text-sm-center text-lg-left">
-                        <a href="#" class="text-white">Risikohinweis</a>
-                        <a href="#" class="text-white">Blog</a>
-                        <a href="#" class="text-white">Presse</a>
-                        <a href="#" class="text-white">Impressum</a>
-                        <a href="#" class="text-white">Hilfe</a>
-                    </div>
                 </div>
-            </div>
             </section>
             <script src="js/jquery.min.js"></script>
             <script src="js/popper.min.js"></script>
