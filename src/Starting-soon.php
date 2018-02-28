83c07259d20c08ca3b7676bbad312797e5658f43
Milos Pavlovic <milosar7@gmail.com>
2018-02-28 09:35:59 +0100

    Files destination change

--- src/Starting-soon.php
+++ src/Starting-soon.php
@@ -1,8 +1,8 @@
 <?php include 'header.php';?>
 <main>
     <div class="container">
-        <a href="" class="d-block mb-4 w-50">
-            <div class="project-single card">
+        <a href="" class="card-clickable-link d-block mb-4 w-50">
+            <div class="starting-soon-single card">
                 <img src="images/armywings/armywings.jpg" class="card-img-top">
                 <div class="card-header bg-primary px-3 py-2 d-flex justify-content-between">
                     <p class="text-uppercase text-white font-weight-bold my-auto">Starting in 1 month</p>
@@ -13,14 +13,14 @@
                     <p class="card-text mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dignissimos a vitae recusandae amet itaque perferendis architecto non aliquam maiores. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit aspernatur possimus neque eveniet natus, illo debitis dolore dolorem sequi aperiam fuga? Voluptatum velit, repudiandae consequatur doloremque error culpa quasi itaque?</p>
                     <hr>
                     <div class="status-wrapper text-dark d-flex">
-                        <div class="scale rounded-circle border border-secondary p-4 pt-5 mr-3 text-center">
-                            <p class="font-weight-bold m-0">0$</p>
-                            <p class="m-0 text-muted">0 Investors</p>
-                            <p>0%</p>
+                        <div class="status-scale rounded-circle border border-secondary p-4 pt-5 mr-3 text-center">
+                            <p class="total-amount font-weight-bold m-0">0$</p>
+                            <p class="numb-of-investors m-0 text-muted">0 Investors</p>
+                            <p class="completed" >0%</p>
                         </div>
-                        <div class="status align-self-center">
-                            <p class="text-muted"><span class="d-block h5 m-0 text-dark">75.000$</span> Minimum Investment</p>
-                            <p class="text-muted"><span class="d-block h5 m-0 text-dark">700.000$</span> Maximum investment</p>
+                        <div class="invested-amount align-self-center">
+                            <p class="text-muted"><span class="min-amoun d-block h5 m-0 text-dark">75.000$</span> Minimum Investment</p>
+                            <p class="text-muted"><span class="max-amount d-block h5 m-0 text-dark">700.000$</span> Maximum investment</p>
                         </div>
                     </div>
                 </div>
