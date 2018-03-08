511d9b7593c569028b8bb023bfca8b25e6076ac3
Milos Pavlovic <milosar7@gmail.com>
2018-03-08 12:48:42 +0100

    Campaign card univ

--- card.php
+++ card.php
@@ -8,11 +8,15 @@
             <!--SINGLE-CAMPAIGN-->
             <div class="col-12 col-md-6">
                 <div class="campaign card mb-4">
-                    <img src="images/armywings/armywings.jpg" class="card-img-top campaign-image">
-                    <div class="campaign-header card-header bg-primary px-3 py-2 d-flex justify-content-between">
-                        <p class="campaign-status text-uppercase text-white font-weight-bold my-auto mr-3">Starting in<span class="h4"> 1 </span> month</p>
-                        <img src="images/armywings/wingslogo.jpg" class="rounded-circle campaign-logo">
-                    </div>
+                    <!--CAMPAIGN IMAGE AND STATUS -->
+                    <a href="#">
+                        <img src="images/armywings/armywings.jpg" class="card-img-top campaign-image">
+                        <div class="campaign-header card-header bg-primary px-3 py-2 d-flex justify-content-between">
+                            <p class="campaign-status text-uppercase text-white font-weight-bold my-auto mr-3">Starting in<span class="h4"> 1 </span> month</p>
+                            <img src="images/armywings/wingslogo.jpg" class="rounded-circle campaign-logo">
+                        </div>
+                    </a>
+                    <!-- CAMPAIGN TITLE AND EXCERT(SUMMARY ABOUT COMPANY), WITH INVESTMENT STATUS-->
                     <div class="campaign-content card-body text-dark bg-light">
                         <div class="card-title d-flex justify-content-start align-items-center">
                             <img src="images/flags/aut.jpg" class="img-fluid mr-3 flag">
@@ -23,6 +27,7 @@
                             neque eveniet natus, illo debitis dolore dolorem sequi aperiam fuga? Voluptatum velit, repudiandae consequatur
                             doloremque error culpa quasi itaque?</p>
                         <hr>
+                        <!-- CAMPAIGN KNOB(SCALE) WITH CURRENT INVESTMENT STATUS AND BASIC INFO-->
                         <div class="campaign-invest-wrapper text-dark d-flex flex-column flex-sm-row justify-content-around">
                             <div class="campaign-invest-scale rounded-circle border border-secondary p-3  mr-3 text-center">
                                 <p class="investment-total-amount font-weight-bold m-0 mt-3">220.500&euro;</p>
@@ -35,35 +40,36 @@
                             </div>
                         </div>       
                     </div>
+                    <!-- CAMPAIGN ADDITIONAL INFO, CELLS WITH ICONS AND BUTTON FOR MORE INFO -->
                     <div class="card-footer p-0">
-                        <div class="campaign-invest-info py-1 d-flex justify-content-around border-bottom border-muted">
-                            <div class="info-cell px-2 py-2 d-flex flex-row">
+                        <div class="campaign-invest-info py-1 d-flex flex-wrap justify-content-around border-bottom border-muted">
+                            <div class="info-cell mx-1 px-2 py-2 d-flex flex-row">
                                 <i class="far fa-star text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="Special project"></i>
                             </div>
-                            <div class="info-cell px-2 py-2 text-center">
+                            <div class="info-cell mx-1 px-2 py-2 text-center">
                                 <i class="fas fa-calendar-alt text-muted fa-1x mb-2" data-toggle="tooltip" data-placement="top" title="Contract term"></i>
                                 <span class="d-block text-muted font-weight-bold">2 years</span>
                             </div>
-                            <div class="info-cell px-2 py-2 text-center">
+                            <div class="info-cell mx-1 px-2 py-2 text-center">
                                 <i class="fas fa-chart-line text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="Base rate"></i>
                                 <span class="d-block text-muted font-weight-bold">2 years</span>
                             </div>
-                            <div class="info-cell px-2 py-2 d-flex flex-row">
+                            <div class="info-cell mx-1 px-2 py-2 d-flex flex-row">
                                 <i class="fas fa-rocket text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="Startup"></i>
                             </div>
-                            <div class="info-cell px-2 py-2 d-flex flex-row">
+                            <div class="info-cell mx-1 px-2 py-2 d-flex flex-row">
                                 <i class="fas fa-users text-muted fa-2x align-self-center" data-toggle="tooltip" data-placement="top" title="KMU"></i>
                             </div>
                         </div>
                         <div class="campaign-invest-info py-1 px-3 border-bottom border-muted">
                             <div class="row">
-                                <div class="col-6 col-md-8 my-2 d-flex justify-content-around">
-                                    <p class="font-weight-bold d-inline-block m-0 d-flex justify-content-center">1 hero token =
-                                        <img src="images/campaigns/eth3.svg" class="pl-4">
+                                <div class="col-6 my-2 d-flex justify-content-around border-right border-muted">
+                                    <p class="font-weight-bold d-inline-block m-0 d-flex justify-content-center">1 HERO token =
+                                        <img src="images/campaigns/eth3.svg" class="pl-1">
                                     </p>
                                     <img src="" alt="">
                                 </div>
-                                <div class="col-6 col-md-4 my-2 d-flex justify-content-between">
+                                <div class="col-6 my-2 d-flex justify-content-around">
                                     <p class="font-weight-bold d-inline-block m-0">0.5</p>
                                     <p class="font-weight-bold d-inline-block m-0">
                                         <span class="text-muted">ETH</span>
